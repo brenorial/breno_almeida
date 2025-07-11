5d2fd243f6f0f31adef77188e4cc0f0e05736f8b
breno rial <brenoalmeida.riosaude@gmail.com>
2025-07-11 11:58:26 -0300

    apontando para o repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/breno_almeida/', // <-- Coloque o NOME DO REPOSITÓRIO AQUI
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
